ae9b45d4bd8e7d9ffcd4d6fffac4d2c53620a7a8
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-23 15:30:44 +0200

    add serveAddGameRequest method in GameController

--- src/main/java/com/example/gamebase/RequestUrlMapper.java
+++ src/main/java/com/example/gamebase/RequestUrlMapper.java
@@ -1,5 +1,6 @@
 package com.example.gamebase;
 
+import com.example.gamebase.controller.GameController;
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
@@ -16,18 +17,20 @@ public class RequestUrlMapper {
     private final static String ADD_GAME_REVIEW_URL = "/game/addReview";
     private final static String ADD_GAME_RATING_URL = "/game/addRating";
 
+    private GameController gameController = new GameController();
+
     public Response delegateRequest(IHTTPSession session){
 
         if (POST.equals(session.getMethod()) && ADD_GAME_URL.equals(session.getUri())){
-            return null;
+            return gameController.serveAddGameRequest(session);
         } else if (GET.equals(session.getMethod()) && GET_ALL_GAME_URL.equals(session.getUri())) {
-            return null;
+            return gameController.serveGetGamesRequest(session);
         } else if(GET.equals(session.getMethod()) && GET_GAME_URL.equals(session.getUri())){
-            return null;
+            return gameController.serveAddGameRequest(session);
         } else if(POST.equals(session.getMethod()) && ADD_GAME_REVIEW_URL.equals(session.getUri())){
-            return null;
+            return gameController.serveAddReviewRequest(session);
         } else if (POST.equals(session.getMethod()) && ADD_GAME_RATING_URL.equals(session.getUri())){
-            return null;
+            return gameController.serveAddRatingRequest(session);
         }
 
         return NanoHTTPD.newFixedLengthResponse(NOT_FOUND,"text/plain" ,"Not found" );
--- src/main/java/com/example/gamebase/controller/GameController.java
+++ src/main/java/com/example/gamebase/controller/GameController.java
@@ -2,8 +2,16 @@ package com.example.gamebase.controller;
 
 import com.example.gamebase.storage.GameStorage;
 import com.example.gamebase.storage.impl.StaticListGameStorageImpl;
+import com.example.gamebase.type.Game;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
+
+import static fi.iki.elonen.NanoHTTPD.Response.Status.OK;
+import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
+import static fi.iki.elonen.NanoHTTPD.Response.Status.INTERNAL_ERROR;
+
 
 public class GameController {
 
@@ -14,11 +22,38 @@ public class GameController {
     }
 
     public Response serveGetGamesRequest(IHTTPSession session){
-        return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        String response = "";
+
+        try {
+            response = objectMapper.writeValueAsString(gameStorage.getAllGames());
+        } catch (JsonProcessingException e){
+            System.err.println("Error during process request \n" + e );
+            return newFixedLengthResponse(INTERNAL_ERROR, "text/plain","Internal server error, can't read all games");
+        }
+
+        return newFixedLengthResponse(OK, "application/json", response);
     }
 
     public Response serveAddGameRequest(IHTTPSession session){
-        return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        long randomGameId = System.currentTimeMillis();
+
+        String lengthHeader = session.getHeaders().get("content-lenght");
+        int contentLenght = Integer.parseInt(lengthHeader);
+        byte[] buffer = new byte[contentLenght];
+
+        try {
+            session.getInputStream().read(buffer,0, contentLenght);
+            String requestBody = new String(buffer).trim();
+            Game requestGame = objectMapper.readValue(requestBody, Game.class);
+            requestGame.setId(randomGameId);
+            gameStorage.addGame(requestGame);
+        } catch (Exception e){
+            System.err.println("Error during process request \n" + e);
+            return newFixedLengthResponse(INTERNAL_ERROR, "text/plain", "Internal error, game hasn't been added");
+        }
+        return newFixedLengthResponse(OK, "text/plain", "Game has been successfully added, id: " + randomGameId );
     }
 
     public Response serveAddReviewRequest(IHTTPSession session){
