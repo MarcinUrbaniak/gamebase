9ffbbb0846336d1eabebfd2796a1eca018a02412
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-23 10:38:00 +0200

    add delegateRequest method in RequestMapper class

--- src/main/java/com/example/gamebase/RequestUrlMapper.java
+++ src/main/java/com/example/gamebase/RequestUrlMapper.java
@@ -1,9 +1,32 @@
 package com.example.gamebase;
 
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.IHTTPSession;
+import fi.iki.elonen.NanoHTTPD.Response;
+
+import static fi.iki.elonen.NanoHTTPD.Method.GET;
+import static fi.iki.elonen.NanoHTTPD.Method.POST;
+import static fi.iki.elonen.NanoHTTPD.Response.Status.NOT_FOUND;
+
 public class RequestUrlMapper {
 
     private final static String ADD_GAME_URL = "/game/add";
     private final static String GET_ALL_GAME_URL = "/game/getAll";
     private final static String ADD_GAME_REVIEW_URL = "/game/addReview";
     private final static String ADD_GAME_RATING_URL = "/game/addRating";
+
+    public Response delegateRequest(IHTTPSession session){
+
+        if (POST.equals(session.getMethod()) && ADD_GAME_URL.equals(session.getUri())){
+            return null;
+        } else if (GET.equals(session.getMethod()) && GET_ALL_GAME_URL.equals(session.getUri())){
+            return null;
+        } else if(POST.equals(session.getMethod()) && ADD_GAME_REVIEW_URL.equals(session.getUri())){
+            return null;
+        } else if (POST.equals(session.getMethod()) && ADD_GAME_RATING_URL.equals(session.getUri())){
+            return null;
+        }
+
+        return NanoHTTPD.newFixedLengthResponse(NOT_FOUND,"text/plain" ,"Not found" );
+    }
 }
